7ab3c2ed5ca70a22084f679f35491260261b2607
Randy <randyarbolaez0@gmail.com>
2020-12-29 11:21:55 -0500

    hope it works backend | removed line 5

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var path = require("path");
 var server = require("http").createServer(app);
-var io = require("socket.io")(server, { origins: "*:*" });
+var io = require("socket.io")(server);
 var port = process.env.PORT || 3000;
 
 server.listen(port, () => {
