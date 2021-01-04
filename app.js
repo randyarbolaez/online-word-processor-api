dbf70fe98fb11ae9d3654b2df62bd85c0d17a8ec
Randy <randyarbolaez0@gmail.com>
2021-01-04 13:28:08 -0500

    done

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-var express = require("express");
-var app = express();
-var path = require("path");
-var server = require("http").createServer(app);
-var io = require("socket.io")(server);
-var port = process.env.PORT || 3000;
+require("dotenv").config();
 
-server.listen(port, () => {
-  console.log(`Server listening at port http://localhost:${port}`);
+const express = require("express");
+const app = express();
+const path = require("path");
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+
+server.listen(process.env.PORT, () => {
+  console.log(`Server listening at port http://localhost:${process.env.PORT}`);
 });
-
-// Chatroom
 
 let allUsersObj = {};
 
 io.on("connection", (socket) => {
-  var addedUser = false;
+  let addedUser = false;
   socket.on("new message", ({ message, id }) => {
     io.to(id).emit("new message", {
       username: socket.username,
@@ -22,7 +21,6 @@ io.on("connection", (socket) => {
     });
   });
 
-  // when the client emits 'add user', this listens and executes
   socket.on("add user", (data) => {
     if (addedUser) return;
     let { username } = data;
@@ -38,7 +36,6 @@ io.on("connection", (socket) => {
 
     addedUser = true;
 
-    console.log(allUsersObj[id].length, " allUsersObj[id].length");
     socket.emit("login", {
       numUsers: allUsersObj[id].length,
       allUsers: allUsersObj[id],
@@ -51,33 +48,12 @@ io.on("connection", (socket) => {
     });
   });
 
-  // when the client emits 'typing', we broadcast it to others
-  socket.on("typing", () => {
-    socket.broadcast.emit("typing", {
-      username: socket.username,
-    });
-  });
-
-  // when the client emits 'stop typing', we broadcast it to others
-  socket.on("stop typing", () => {
-    socket.broadcast.emit("stop typing", {
-      username: socket.username,
-    });
-  });
-
-  // when the user disconnects.. perform this
   socket.on("disconnect", () => {
     if (addedUser) {
       let indexOfUserThatLeft = allUsersObj[socket.roomID].indexOf(
         socket.username
       );
       allUsersObj[socket.roomID].splice(indexOfUserThatLeft, 1);
-      console.log(allUsersObj[socket.roomID]);
-      // socket.broadcast.emit("user left", {
-      //   username: socket.username,
-      //   numUsers: numUsers,
-      //   usersLeft: allUsers,
-      // });
 
       io.to(socket.roomID).emit("user left", {
         username: socket.username,
